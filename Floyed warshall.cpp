f973f1022aef0c07c49f21889fda27f7b1f3f1f9
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2020-11-20 19:53:55 +0600

    Update and rename floyed warshall.cpp to Floyed warshall.cpp

--- Floyed warshall.cpp
+++ Floyed warshall.cpp
@@ -1,38 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define pb push_back
-#define ff first
-#define ss second
-#define EMPTY_VALUE -1
-#define inf  1<<28
-#define CLR(x,y) memset( x,y,sizeof(x))
-#define FOR(x,n) for(int i = x; i<n; i++)
-#define read(x) freopen(x, "r", stdin);
-#define write(x) freopen( x, "w", stdout);
-
-const int MOD = 1e9+7;
 const int mx = 1e4+9;
-
-typedef long long ll;
-typedef vector<int> v;
-typedef pair<int,int> pii;
-typedef map<string,int> mp;
-
-int fx[] = {+0, +0, +1, -1};
-int fy[] = {+1, -1, +0, +0};
 
 int adj[mx][mx];
 int n, e;
 
 int main() {
-
-    read( "in.txt");
+    //freopen( "in.txt", "r", stdin);
+    
     cin >> n >> e;
     for (int i = 0; i<n; i++) {
         for (int j = 0; j<n; j++) {
             if ( i==j) adj[i][j] = 0;
-            else adj[i][j]= inf;
+            else adj[i][j]= INT_MAX;
         }
     }
     for (int i = 0; i<e; i++) {
